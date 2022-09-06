cd467ce4f622c9b476bcf1a894e42c3e191fd3a4
Hanzala_Iqbal <hanzalaiqbal313@gmail.com>
2022-09-06 23:14:28 +0500

    Nav Drawer fixes!

--- app/src/main/java/com/example/quraan_app/ayahActivity.java
+++ app/src/main/java/com/example/quraan_app/ayahActivity.java
@@ -1,20 +1,41 @@
 package com.example.quraan_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.database.SQLException;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class ayahActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
 
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+            Toast.makeText(getApplicationContext(),"Start",Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"End",Toast.LENGTH_LONG).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +45,49 @@ public class ayahActivity extends AppCompatActivity {
         int index=Integer.parseInt(intent.getStringExtra("surahIndex"));
         Toast.makeText(this, "Index: " + index, Toast.LENGTH_SHORT).show();
 
+        NavigationView navigationView;
+        DrawerLayout drawerLayout;
+        Toolbar toolbar;
+        ActionBarDrawerToggle toggle;
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.read_quran :
+                        Intent intent1 = new Intent(ayahActivity.this, bynameList.class);
+                        startActivity(intent1);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.home :
+                        Intent intent2 = new Intent(ayahActivity.this, MainActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.search:
+                        Intent intent3 = new Intent(ayahActivity.this, SearchFunction.class);
+                        startActivity(intent3);
+                        break;
+
+                }
+
+                return true;
+            }
+        });
 
         DBMain db;
         db = new DBMain(getApplicationContext());
